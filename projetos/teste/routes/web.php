0c99152a3ca2bee7b48186962341ebcdb2252ae4
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-25 18:24:16 -0300

    18. Métodos HTTP

--- projetos/teste/routes/web.php
+++ projetos/teste/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use Facade\FlareClient\View;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -71,4 +72,34 @@ Route::get('/produtos', function()
 Route::get('todosprodutos2', function ()
 {
     return redirect()->route('meusprodutos');
+});
+
+Route::post('/requisicoes', function (Request $request)
+{
+    return 'Hello POST';    
+});
+
+Route::delete('/requisicoes', function (Request $request)
+{
+    return 'Hello DELETE';    
+});
+
+Route::put('/requisicoes', function (Request $request)
+{
+    return 'Hello PUT';    
+});
+
+Route::patch('/requisicoes', function (Request $request)
+{
+    return 'Hello PATCH';    
+});
+
+Route::options('/requisicoes', function (Request $request)
+{
+    return 'Hello OPTIONS';    
+});
+
+Route::get('/requisicoes', function (Request $request)
+{
+    return 'Hello GET';    
 });
