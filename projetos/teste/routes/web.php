2476440672d4e5582d0c21fe4cd86b9c45ebf93c
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-27 18:57:18 -0300

    43. Utilizando o Bootstrap 4

--- projetos/teste/routes/web.php
+++ projetos/teste/routes/web.php
@@ -31,6 +31,10 @@ Route::get('multiplicar/{n1}/{n2}', 'ClienteController@multiplicar');
 Route::get('opcoes/{opcao?}', function($opcao=null){
     return view('outras.opcoes', compact(['opcao']));
 })->name('opcoes');
+Route::get('bootstrap', function()
+{
+    return view('outras.exemplo');
+});
 
 
 Route::resource('clientes', 'ClienteController');
