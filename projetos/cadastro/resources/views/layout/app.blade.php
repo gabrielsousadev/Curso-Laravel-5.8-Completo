a79fa2f45b5089c7dce4064f613aac83a4537a22
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-28 16:12:02 -0300

    68. Importante: O Token CSRF

--- projetos/cadastro/resources/views/layout/app.blade.php
+++ projetos/cadastro/resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <title>Cadastro de Produtos</title>
 </head>
 <body>
