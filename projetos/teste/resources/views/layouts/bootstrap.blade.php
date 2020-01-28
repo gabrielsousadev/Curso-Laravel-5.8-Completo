df153a074af2d9892f860987c6d6f91fc44bb3d6
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-28 09:12:48 -0300

    43. Utilizando o Bootstrap 4

--- projetos/teste/resources/views/layouts/bootstrap.blade.php
+++ projetos/teste/resources/views/layouts/bootstrap.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
+    <meta name="csrf-token" content="{{csrf_token()}}">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.6">
     <title>Pricing example · Bootstrap</title>
