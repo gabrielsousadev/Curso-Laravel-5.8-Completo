b264e7a280ee657a1d5103a86feea94aa2b8bd81
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-31 10:51:27 -0300

    86. Validação - Min/Max

--- projetos/validacao/app/Http/Controllers/ClienteControlador.php
+++ projetos/validacao/app/Http/Controllers/ClienteControlador.php
@@ -37,7 +37,7 @@ class ClienteControlador extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'nome'=> 'required'
+            'nome'=> 'required | min:5 | max:10'
         ]);
 
         $cliente = new Cliente();
