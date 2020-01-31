3b2d0279104fab364489cbd9e07b0e244fdf3c57
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-01-31 14:00:41 -0300

    90. Customização de mensagens - campo a campo // 91. Resolução do exercício

--- projetos/validacao/resources/views/novocliente.blade.php
+++ projetos/validacao/resources/views/novocliente.blade.php
@@ -29,33 +29,44 @@
                             @csrf
                             <div class="form-group">
                                 <label for="nome">Nome do Cliente</label>
-                                <input type="text" id="id_nome" class="form-control" name="nome" placeholder="Nome do cliente">
+                            <input type="text" id="id_nome" class="form-control {{$errors->has('nome') ? 'is-invalid' : ''}}" name="nome" placeholder="Nome do cliente">
+                                @if ($errors->has('nome'))
+                                <div class="invalid-feedback">
+                                    {{$errors->first('nome')}}
+                                </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="nome">Idade do Cliente</label>
-                                <input type="number" id="id_idade" class="form-control" name="idade" placeholder="Idade do cliente">
+                                <input type="number" id="id_idade" class="form-control {{$errors->has('idade') ? 'is-invalid' : ''}}" name="idade" placeholder="Idade do cliente">
+                                @if ($errors->has('idade'))
+                                <div class="invalid-feedback">
+                                    {{$errors->first('idade')}}
+                                </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="nome">Endereço do Cliente</label>
-                                <input type="text" id="id_idade" class="form-control" name="endereco" placeholder="Endereço do cliente">
+                                <input type="text" id="id_idade" class="form-control {{$errors->has('endereco') ? 'is-invalid' : ''}}" name="endereco" placeholder="Endereço do cliente">
+                                @if ($errors->has('endereco'))
+                                <div class="invalid-feedback">
+                                    {{$errors->first('endereco')}}
+                                </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="nome">Email do Cliente</label>
-                                <input type="text" id="id_email" class="form-control" name="email" placeholder="E-mail do cliente">
+                                <input type="text" id="id_email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" name="email" placeholder="E-mail do cliente">
+                                @if ($errors->has('email'))
+                                <div class="invalid-feedback">
+                                    {{$errors->first('email')}}
+                                </div>
+                                @endif
                             </div>
                             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                             <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
                         </form>
                     </div>
-                    @if ($errors->any())
-                    <div class="card-footer">
-                        @foreach ($errors->all() as $error)
-                            <div class="alert alert-danger" role="alert">
-                                {{$error}}
-                            </div>
-                        @endforeach
-                    </div>
-                    @endif
                 </div>
             </div>
         </div>
