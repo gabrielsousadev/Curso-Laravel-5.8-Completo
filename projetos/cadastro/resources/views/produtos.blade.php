d9ac4aa8865ea921d64cc0eacf968ee71179ddba
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-05 10:20:47 -0300

    95. HTML - View Produto - Formulário para salvar Produtos
    $('#id_dialogProdutos')
    $('#id_dialogProdutos').modal('show')
    $('#id_dialogProdutos').modal('hide')

--- projetos/cadastro/resources/views/produtos.blade.php
+++ projetos/cadastro/resources/views/produtos.blade.php
@@ -24,4 +24,53 @@
         <button class="btn btn-sm btn-primary" role="button">Novo Produto</a>
     </div>
 </div>
+
+<div class="modal" tabindex="-1" role="dialog" id="id_dialogProdutos">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form class="form-horizontal" id="id_formProduto">
+                <div class="modal-header">
+                    <h5 class="modal-title">Novo Produto</h5>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" id="id" class="form-control">
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label">Nome do Produto</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="precoProduto" class="control-label">Preço</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="precoProduto" placeholder="Preço do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="quantidadeProduto" class="control-label">Quantidade</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="quantidadeProduto" placeholder="Quantidade do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="departamentoProduto" class="control-label">Nome do Produto</label>
+                        <div class="input-group">
+                            <select class="form-control" id="departamentoProduto">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                        <button type="cancel" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 @endsection
