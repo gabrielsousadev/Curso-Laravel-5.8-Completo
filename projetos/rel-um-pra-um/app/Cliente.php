9723ae45cfa7f7adf533fc24d00632998ccbe933
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-13 11:37:58 -0300

    113. Utilizando hasOne

--- projetos/rel-um-pra-um/app/Cliente.php
+++ projetos/rel-um-pra-um/app/Cliente.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    public function endereco()
+    {
+        return $this->hasOne('App\Endereco'); //Buscar um endereco dentro da tabela endereco
+    }
 }
